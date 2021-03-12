e039e8fe6712a15c13980eeac2968f92e828da09
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-03-12 14:32:36 +0800

    Added redirect from http to https

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -23,7 +23,8 @@
     git clone --depth 1 {{ $repository }}
     cd {{ $project_dir }}
     git reset --hard {{ $commit }}
-    sed -i 's@APP_URL=.*@APP_URL={{ $appUrl }}@g' env.staging
+    sed -i 's@APP_URL=.*@APP_URL={{ $appUrl }}@g' .env.staging
+    sed -i 's@server_name laravel.test@server_name {{ $serverName }}@g' docker-compose/nginx/sites/default.conf
 @endtask
 
 @task('down_existing_containers')
@@ -35,7 +36,7 @@
 @task('start_containers')
     echo "Starting deployment ({{ $release }})"
     cd {{ $project_dir }}
-    docker-compose -f docker-compose.staging.yml --env-file ./env.staging up -d --force-recreate --build
+    docker-compose -f docker-compose.staging.yml --env-file ./.env.staging up -d --force-recreate --build
 @endtask
 
 
