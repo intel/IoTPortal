def73ded6c2655f3c7d0b4789ac3450065f5ba9b
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-03-22 00:44:24 +0800

    Debug repo link error

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,7 +1,7 @@
 @servers(['staging' => $stagingSshHost])
 
 @setup
-    $repository = '{{ $projectRepository }}';
+    $repository = {{ $projectRepository }};
     $project_dir = 'i-iot-portal';
     $release = date('YmdHis');
 @endsetup
@@ -21,6 +21,7 @@
 
 @task('clone_repository')
     echo 'Cloning repository'
+    echo {{ $repository }}
     git clone --depth 1 {{ $repository }}
     cd {{ $project_dir }}
     git reset --hard {{ $commit }}
