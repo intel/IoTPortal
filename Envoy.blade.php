c3d9aa909ef3946d770f267a2e981a2d7a91718d
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-04-29 14:36:03 +0800

    Fixed permission denied issue on laravel log

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -29,8 +29,8 @@
     echo 'Building artifacts'
     cd {{ $project_dir }}
     sed -i 's~server_name localhost host.docker.internal~server_name {{ $serverName }}~g' docker-compose/nginx/sites/default.conf
-    sed -i 's~allow 127.0.0.1~allow {{ $unblockIp }}~g' docker-compose/nginx/sites/default.conf
-    sed -i 's~allow 127.0.0.2~allow {{ $serverIp }}~g' docker-compose/nginx/sites/default.conf
+{{--    sed -i 's~allow 127.0.0.1~allow {{ $unblockIp }}~g' docker-compose/nginx/sites/default.conf--}}
+{{--    sed -i 's~allow 127.0.0.2~allow {{ $serverIp }}~g' docker-compose/nginx/sites/default.conf--}}
     sed -i 's~APP_URL=.*~APP_URL={{ $appUrl }}~g' .env.staging
     sed -i 's~DB_PASSWORD=.*~DB_PASSWORD={{ $dbPassword }}~g' .env.staging
     sed -i 's~MQTT_HOST=.*~MQTT_HOST={{ $mqttHost }}~g' .env.staging
