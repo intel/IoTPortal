47d88757e5c90825ca432f1f61868f1eec839058
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-05-01 22:07:04 +0800

    Fixed deployment

--- docker-compose/build/setup.sh
+++ docker-compose/build/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp -r /certs /iotportaldata/ca-certificates
+cp -r /certs/. /iotportaldata/ca-certificates
 
-cp -r /certs /iotportaldata/ssl
+cp -r /certs/. /iotportaldata/ssl
 
 cp /var/www/docker-compose/nginx/nginx.conf /iotportaldata/nginx
 
-cp -r /var/www/docker-compose/nginx/sites /iotportaldata/nginx/sites-available
+cp -r /var/www/docker-compose/nginx/sites/* /iotportaldata/nginx/sites-available
 
-cp /var/www/.env /iotportaldata/env/.env
+cp /var/www/.env /iotportaldata/env
