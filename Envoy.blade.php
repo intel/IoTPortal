311913b4063f7a7cab95b413b94cffea2c251929
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-05-02 17:26:07 +0800

    Deployment test 4

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -65,15 +65,15 @@
     OUTPUT_DIR="$( cd iotportaldata && pwd )"
     echo $OUTPUT_DIR
     ENV_DIR="$OUTPUT_DIR/env"
-{{--    if ! grep -q "^LOCAL_UID=" $ENV_DIR/uid.env 2>/dev/null || ! grep -q "^LOCAL_GID=" $ENV_DIR/uid.env 2>/dev/null--}}
-{{--    then--}}
-{{--        LUID="LOCAL_UID=`id -u $USER`"--}}
-{{--        [ "$LUID" == "LOCAL_UID=0" ] && LUID="LOCAL_UID=65534"--}}
-{{--        LGID="LOCAL_GID=`id -g $USER`"--}}
-{{--        [ "$LGID" == "LOCAL_GID=0" ] && LGID="LOCAL_GID=65534"--}}
-{{--        echo $LUID >$ENV_DIR/uid.env--}}
-{{--        echo $LGID >>$ENV_DIR/uid.env--}}
-{{--    fi--}}
+    if ! grep -q "^LOCAL_UID=" $ENV_DIR/uid.env 2>/dev/null || ! grep -q "^LOCAL_GID=" $ENV_DIR/uid.env 2>/dev/null
+    then
+        LUID="LOCAL_UID=`id -u $USER`"
+        [ "$LUID" == "LOCAL_UID=0" ] && LUID="LOCAL_UID=65534"
+        LGID="LOCAL_GID=`id -g $USER`"
+        [ "$LGID" == "LOCAL_GID=0" ] && LGID="LOCAL_GID=65534"
+        echo $LUID >$ENV_DIR/uid.env
+        echo $LGID >>$ENV_DIR/uid.env
+    fi
 @endtask
 
 @task('build_artifacts')
