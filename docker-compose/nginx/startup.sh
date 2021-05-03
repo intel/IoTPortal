6e1b208975e98fc963091d7c5fb17c15b560645d
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-05-03 20:12:42 +0800

    Deployment test 19

--- docker-compose/nginx/startup.sh
+++ docker-compose/nginx/startup.sh
@@ -13,18 +13,3 @@ crond -l 2 -b
 
 # Start nginx in foreground
 nginx
-
-
-#if [ ! -f /etc/nginx/ssl/default.crt ]; then
-#    openssl req -new -newkey rsa:2048 -nodes -out localhost.csr -keyout localhost.key -subj "/C=US/ST=Oregon/L=Oregon/O=default/OU=default/CN=localhost"
-##    openssl genrsa -out "/etc/nginx/ssl/default.key" 2048
-##    openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=host.docker.internal/O=default/C=US"
-##    openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
-#    chmod 644 /etc/nginx/ssl/localhost.key
-#fi
-#
-## Start crond in background
-#crond -l 2 -b
-#
-## Start nginx in foreground
-#nginx
--- resources/js/redux/store.js
+++ resources/js/redux/store.js
@@ -7,9 +7,9 @@ import rootReducer from './root.reducer';
 
 const middlewares = [thunk];
 
-// if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
-// }
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
