953028c13c0fb04199e5ce75363e530037f7ca20
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-05-03 19:35:43 +0800

    Deployment test 18

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="version" content="90354">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Intel IoT Portal</title>
