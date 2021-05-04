9e756d8b99b4e05dd4b6b12effe2b8ddd9c83fa9
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-05-04 19:04:17 +0800

    wait-for-mysql test 7

--- docker-compose/app/wait-for-mysql.sh
+++ docker-compose/app/wait-for-mysql.sh
@@ -6,16 +6,13 @@ host="$1"
 shift
 cmd="$@"
 
-echo $host
-echo $MYSQL_ROOT_PASSWORD
 
-mysqladmin ping -h "$host" -P 3306 -u root --password="$MYSQL_ROOT_PASSWORD"
-
-until out=$$(mysqladmin ping -h "$host" -P 3306 -u root --password="$MYSQL_ROOT_PASSWORD"); echo $out | grep "mysqld is alive"; do
+until out=$(mysqladmin ping -h "$host" -P 3306 -u root --password="$MYSQL_ROOT_PASSWORD"); echo $out | grep "mysqld is alive"; do
   >&2 echo "MySQL is unavailable - sleeping"
-  echo $out
   sleep 1
 done
 
 >&2 echo "MySQL is up - executing command"
 exec $cmd
+
+
