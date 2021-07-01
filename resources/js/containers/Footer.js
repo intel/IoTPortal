94f7935b0938439f5c90462ecbd9050e64f7ac1b
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-07-01 15:55:29 +0800

    Fixed portal naming

--- resources/js/containers/Footer.js
+++ resources/js/containers/Footer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { CFooter } from '@coreui/react';
 
 const Footer = () => {
-    return (
-        <CFooter fixed={false}>
-            <div>
-                <a href="https://www.intel.com" target="_blank" rel="noopener noreferrer">Intel IoT Portal</a>
-                <span className="ml-1">&copy; 2021 Intel.</span>
-            </div>
-        </CFooter>
-    );
+  return (
+    <CFooter fixed={false}>
+      <div>
+        <a href="https://www.intel.com" target="_blank" rel="noopener noreferrer">IoT Portal</a>
+        <span className="ml-1">&copy; 2021 Intel.</span>
+      </div>
+    </CFooter>
+  );
 };
 
 export default React.memo(Footer);
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Intel IoT Portal</title>
+    <title>IoT Portal</title>
     <!-- Styles -->
     @include('includes.css')
     {{--    <link href="{{ asset('css/style.css') }}" rel="stylesheet">--}}
