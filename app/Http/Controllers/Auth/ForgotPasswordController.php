e78623f22f56d2b629bc0c0b0d52cd63b1269d93
Sow, Chun Jieh <chun.jieh.sow@intel.com>
2021-09-01 22:06:53 +0800

    Throttle forgot password route and return success even for invalid emails

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +23,36 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    public function __construct()
+    {
+        $this->middleware('throttle:5,1', ['except' => 'showLinkRequestForm']);
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param Request $request
+     * @return RedirectResponse|JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
+        );
+
+        // Prevent timing attack to enumerate users.
+        usleep(500000 + random_int(0, 1500000));
+
+        // Treat all three cases 'invalid user', 'reset link sent', and 'reset link not sent due to throttling'
+        // the same, with a suitable message, again, to prevent user enumeration attack.
+        return in_array($response, [Password::INVALID_USER, Password::RESET_LINK_SENT, Password::RESET_THROTTLED])
+            ? $this->sendResetLinkResponse($request, Password::RESET_LINK_SENT)
+            : $this->sendResetLinkFailedResponse($request, $response);
+    }
 }
